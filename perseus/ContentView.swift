53d0a0fce520af979ca770f21e0d44525c89ddee
lufazol <luanfazolin@gmail.com>
2023-08-23 11:13:40 -0300

    Implement interface

--- perseus/ContentView.swift
+++ perseus/ContentView.swift
@@ -34,7 +34,6 @@ struct ContentView: View {
 
             NavigationView {
                 DadosView()
-                    .navigationTitle("Dados")
             }
             .tabItem {
                 Image(systemName: "person.text.rectangle")
--- perseus/DadosView.swift
+++ perseus/DadosView.swift
@@ -7,11 +7,159 @@
 
 import SwiftUI
 
-struct DadosView: View {
-    var body: some View {
-        Text("Dados View")
+extension Color {
+    init(hex: UInt, alpha: Double = 1.0) {
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xff) / 255,
+            green: Double((hex >> 8) & 0xff) / 255,
+            blue: Double(hex & 0xff) / 255,
+            opacity: alpha
+        )
     }
 }
+
+struct DadosView: View {
+    var body: some View {
+        
+        let rectangleWidth = CGFloat(32)
+        let rectangleHeight = CGFloat(32)
+
+        VStack {
+            Form {
+                Section {
+                    HStack {
+                        Spacer()
+                        VStack {
+                            Circle()
+                                .frame(
+                                    width: UIScreen.main.bounds.width * 0.2,
+                                    height: UIScreen.main.bounds.height * 0.1
+                                )
+                            Text("Nome do Vovô")
+                                .font(.largeTitle)
+                        }
+                        Spacer()
+                    }
+                }
+                .listRowBackground(Color.clear)
+
+                Section {
+                    HStack {
+                        Image(systemName: "doc.text.below.ecg.fill")
+                            .imageScale(.large)
+                            .foregroundColor(.white)
+                            .background(
+                                Rectangle()
+                                    .foregroundColor(Color(hex: 0x56BABF))
+                                    .frame(width: rectangleWidth, height: rectangleHeight)
+                                    .cornerRadius(10)
+                            )
+                            .padding(.trailing, 10)
+                        Text("Ficha médica")
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .foregroundColor(.gray)
+                    }
+                }
+
+                Section {
+                    HStack {
+                        Image(systemName: "person.line.dotted.person.fill")
+                            .imageScale(.small)
+                            .foregroundColor(.white)
+                            .background(
+                                Rectangle()
+                                    .foregroundColor(Color(hex: 0x459599))
+                                    .frame(width: rectangleWidth, height: rectangleHeight)
+                                    .cornerRadius(10)
+                            )
+                            .padding(.trailing, 10)
+                        Text("Contatos")
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .foregroundColor(.gray)
+                    }
+                }
+
+                Section {
+                    HStack {
+                        Image(systemName: "bell.badge.fill")
+                            .imageScale(.large)
+                            .foregroundColor(.white)
+                            .background(
+                                Rectangle()
+                                    .foregroundColor(Color(hex: 0x928AE5))
+                                    .frame(width: rectangleWidth, height: rectangleHeight)
+                                    .cornerRadius(10)
+                            )
+                            .padding(.trailing, 10)
+                        Text("Notificações")
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .foregroundColor(.gray)
+                    }
+                    HStack {
+                        Image(systemName: "globe.americas.fill")
+                            .imageScale(.large)
+                            .foregroundColor(.white)
+                            .background(
+                                Rectangle()
+                                    .foregroundColor(Color(hex: 0x766CD9))
+                                    .frame(width: rectangleWidth, height: rectangleHeight)
+                                    .cornerRadius(10)
+                            )
+                            .padding(.trailing, 10)
+                        Text("Idiomas")
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .foregroundColor(.gray)
+                    }
+                    HStack {
+                        Image(systemName: "hand.raised.fill")
+                            .imageScale(.large)
+                            .foregroundColor(.white)
+                            .background(
+                                Rectangle()
+                                    .foregroundColor(Color(hex: 0x574CBF))
+                                    .frame(width: rectangleWidth, height: rectangleHeight)
+                                    .cornerRadius(10)
+                            )
+                            .padding(.trailing, 10)
+                        Text("Declaração de privacidade")
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .foregroundColor(.gray)
+                    }
+                }
+
+                Section {
+                    Button(action: {
+                        // Action to perform when the button is tapped
+                    }) {
+                        HStack {
+                            Spacer()
+                            Text("Apagar conta")
+                                .foregroundColor(.red)
+                            Spacer()
+                        }
+
+                    }
+                    .background(Color.white)
+                    .cornerRadius(10)
+                }
+            }
+        }
+        .navigationBarItems(trailing:
+            Button(action: {
+                // Action when the button is tapped
+            }) {
+                Image(systemName: "square.and.arrow.up")
+            }
+        )
+    }
+}
+
 
 struct DadosView_Previews: PreviewProvider {
     static var previews: some View {
