2261f4b0a06c872d065150a4de440c34b7c58442
lufazol <luanfazolin@gmail.com>
2023-08-24 11:36:28 -0300

    Implement FichaView's interface

--- perseus/FichaView.swift
+++ perseus/FichaView.swift
@@ -9,7 +9,81 @@ import SwiftUI
 
 struct FichaView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Form {
+                Section {
+                    HStack {
+                        Spacer()
+                        VStack {
+                            Circle()
+                                .frame(
+                                    width: UIScreen.main.bounds.width * 0.3,
+                                    height: UIScreen.main.bounds.height * 0.1
+                                )
+                        }
+                        Spacer()
+                    }
+                }
+                .listRowBackground(Color.clear)
+                
+                Section {
+                    HStack {
+                        Text("Nome do idoso")
+                        Spacer()
+                        Text("Amélia")
+                    }
+                }
+
+                Section {
+                    HStack {
+                        Text("Data de nascimento")
+                        Spacer()
+                        Text("03/05/1940")
+                    }
+                }
+
+                Section {
+                    HStack {
+                        Text("Sexo")
+                        Spacer()
+                        Text("Feminino")
+                    }
+                }
+
+                Section {
+                    HStack {
+                        Text("Tipo sanguíneo")
+                        Spacer()
+                        Text("Potente D+")
+                    }
+                }
+
+                Section {
+                    HStack {
+                        Text("tuc tuc de metal no coração, & muitas outras")
+                    }
+                } header: {
+                    Text("Cirurgias")
+                }
+
+                Section {
+                    HStack {
+                        Text("Todas as doenças")
+                    }
+                } header: {
+                    Text("Doenças")
+                }
+            }
+        }
+        .navigationBarTitle("Dados")
+        .navigationBarItems(
+            trailing: Button(
+                action: {
+                    // Action when the button is tapped
+                }) {
+                    Text("Editar")
+                }
+        )
     }
 }
 
