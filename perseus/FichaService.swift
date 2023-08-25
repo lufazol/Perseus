a71f525875d5f39d723113174fdd5ae7a77a2724
Vitor Monzillo <66293065+vitormonzillo@users.noreply.github.com>
2023-08-25 12:39:25 -0300

    adiciona persistência de dados e uma view para teste

--- perseus/FichaService.swift
+++ perseus/FichaService.swift
@@ -6,41 +6,92 @@
 //
 
 import Foundation
+import CoreData
 
 class FichaService: ObservableObject {
-    // save fetched notes for view loading
-    @Published var dadosFicha: Ficha
+    @Published var dadosFicha: Ficha?
     
-    let dataService = PersistenceController.shared
-
-    //states
-    @Published var showAlert: Bool = false
-    @Published var noteTitle: String = ""
-    @Published var noteBody: String = ""
-    @Published var noteIsFav: Bool = false
+    let persistence = PersistenceController.shared
 
     init() {
-        getAllDadosDaFicha()
+    }
+    //CRUD
+    func getDadosDaFicha() -> Ficha? {
+        let fetchRequest: NSFetchRequest<Ficha> = Ficha.fetchRequest()
+        
+        do {
+            let fichas = try persistence.container.viewContext.fetch(fetchRequest)
+            return fichas.first
+        } catch {
+            print("Erro ao buscar a ficha: \(error)")
+            return nil
+        }
     }
 
-    func getAllDadosDaFicha() {
-        dadosFicha = dataService.read()
+    func createFicha(nome: String, sexo: String, nascimento: Date, peso: Int64, tipoSanguineo: String, doencas: String, cirurgias: String, alergias: String, id: UUID, createdAt: Date) {
+        
+        //criando idoso
+        let novoIdoso = Idoso(context: persistence.container.viewContext)
+        novoIdoso.id = UUID()
+        novoIdoso.createdAt = Date()
+        
+        //criando ficha
+        let novaFicha = Ficha(context: persistence.container.viewContext)
+        novaFicha.nome = nome
+        novaFicha.sexo = sexo
+        novaFicha.nascimento = nascimento
+        novaFicha.peso = peso
+        novaFicha.tipoSanguineo = tipoSanguineo
+        novaFicha.doencas = doencas
+        novaFicha.cirurgias = cirurgias
+        novaFicha.alergias = alergias
+        novaFicha.id = UUID()
+        novaFicha.createdAt = Date()
+        
+        // Associando a ficha ao idoso criado
+        novoIdoso.ficha = novaFicha
+        novaFicha.idoso = novoIdoso
+    
+        persistence.saveContext()  // Salve as alterações no contexto
     }
 
-    func createFicha() {
-        dataService.create(title: noteTitle, body: noteBody, isFavorite: noteIsFav)
-        getAllDadosDaFicha()
+    func updateFicha(ficha: Ficha, nome: String, sexo: String, nascimento: Date, peso: Int64, tipoSanguineo: String, doencas: String, cirurgias: String, alergias: String) {
+        ficha.nome = nome
+        ficha.sexo = sexo
+        ficha.nascimento = nascimento
+        ficha.peso = peso
+        ficha.tipoSanguineo = tipoSanguineo
+        ficha.doencas = doencas
+        ficha.cirurgias = cirurgias
+        ficha.alergias = alergias
+        
+        persistence.saveContext()
     }
-
-    func deleteNote(ficha: Ficha) {
-        dataService.delete(item: ficha)
-        getAllDadosDaFicha()
+    
+    func deleteFicha(ficha: Ficha) {
+        persistence.delete(item: ficha)
     }
-
-    func clearStates() {
-        showAlert = false
-        noteTitle = ""
-        noteBody = ""
-        noteIsFav = false
+    
+    func deleteALLFICHAS() {
+        let fetchRequest: NSFetchRequest<Ficha> = Ficha.fetchRequest()
+        do {
+            let fichas = try persistence.container.viewContext.fetch(fetchRequest)
+            for ficha in fichas {
+                deleteFicha(ficha: ficha)
+            }
+        } catch {
+            print("Erro ao buscar fichas para deleção: \(error)")
+        }
+    }
+    
+    func getALLFICHAS() -> [Ficha?] {
+        let fetchRequest: NSFetchRequest<Ficha> = Ficha.fetchRequest()
+        do {
+            let fichas = try persistence.container.viewContext.fetch(fetchRequest)
+            return fichas
+        } catch {
+            print("Erro ao buscar fichas para deleção: \(error)")
+            return [nil]
+        }
     }
 }
---
+++ perseus/TesteCoreData.swift
@@ -0,0 +1,75 @@
+//
+//  TesteCoreData.swift
+//  perseus
+//
+//  Created by Vitor Monzillo on 25/08/23.
+//
+
+import SwiftUI
+
+struct TesteCoreData: View {
+    @State private var nome = ""
+    @State private var sexo = ""
+    @State private var nascimento = Date()
+    @State private var peso = ""
+    @State private var tipoSanguineo = ""
+    @State private var doencas = ""
+    @State private var cirurgias = ""
+    @State private var alergias = ""
+    @State private var createdFichaUUID: UUID? = nil
+    
+    @ObservedObject var fichaService = FichaService()
+    
+    var body: some View {
+        VStack {
+            TextField("Nome", text: $nome)
+            TextField("Sexo", text: $sexo)
+            DatePicker("Nascimento", selection: $nascimento)
+            TextField("Peso", text: $peso)
+            TextField("Tipo Sanguíneo", text: $tipoSanguineo)
+            TextField("Doenças", text: $doencas)
+            TextField("Cirurgias", text: $cirurgias)
+            TextField("Alergias", text: $alergias)
+            
+            Button("Criar Ficha") {
+                if let ficha = fichaService.getDadosDaFicha() {
+                    fichaService.updateFicha(ficha: ficha, nome: nome, sexo: sexo, nascimento: nascimento, peso: Int64(peso) ?? 0, tipoSanguineo: tipoSanguineo, doencas: doencas, cirurgias: cirurgias, alergias: alergias)
+                } else {
+                    let newFichaUUID = UUID()
+                    fichaService.createFicha(
+                        nome: nome,
+                        sexo: sexo,
+                        nascimento: nascimento,
+                        peso: Int64(peso) ?? 0,
+                        tipoSanguineo: tipoSanguineo,
+                        doencas: doencas,
+                        cirurgias: cirurgias,
+                        alergias: alergias,
+                        id: newFichaUUID,
+                        createdAt: Date()
+                    )
+                    createdFichaUUID = newFichaUUID
+                }
+            }
+            
+            Button("Buscar Ficha") {
+                if let ficha = fichaService.getDadosDaFicha() {
+                    print("Ficha encontrada:")
+                    print(ficha.nome!)
+                } else {
+                    print("Ficha não encontrada")
+                }
+            }
+
+        }
+        .padding()
+    }
+}
+
+
+
+struct TesteCoreData_Previews: PreviewProvider {
+    static var previews: some View {
+        TesteCoreData()
+    }
+}
