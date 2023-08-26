d5a9d2513cba5d8f99a1b06e692eb548417d642a
lufazol <luanfazolin@gmail.com>
2023-08-26 20:01:42 -0300

    Add images and create Views for app intro

---
+++ perseus/IntroPageModel.swift
@@ -0,0 +1,8 @@
+//
+//  IntroPageModel.swift
+//  perseus
+//
+//  Created by Luan Fazolin on 26/08/23.
+//
+
+import Foundation
---
+++ perseus/PresentIntroView.swift
@@ -0,0 +1,20 @@
+//
+//  PresentIntroView.swift
+//  perseus
+//
+//  Created by Luan Fazolin on 26/08/23.
+//
+
+import SwiftUI
+
+struct PresentIntroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PresentIntroView_Previews: PreviewProvider {
+    static var previews: some View {
+        PresentIntroView()
+    }
+}
