75e21a3e9802eb0b0df082104b497f0e437eead7
lufazol <luanfazolin@gmail.com>
2023-08-29 10:01:39 -0300

    Create DetalheBoletimView

---
+++ perseus/DetalheBoletimView.swift
@@ -0,0 +1,20 @@
+//
+//  DetalheBoletimView.swift
+//  perseus
+//
+//  Created by Luan Fazolin on 29/08/23.
+//
+
+import SwiftUI
+
+struct DetalheBoletimView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetalheBoletimView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetalheBoletimView()
+    }
+}
