d871cf68da8a5fb2ebc07ae3ced32879a6a94280
LPavarini <lucasfinzzeto@hotmail.com>
2023-08-30 11:04:41 -0300

    Show the added drugs

--- perseus/MedicamentosView.swift
+++ perseus/MedicamentosView.swift
@@ -1,15 +1,61 @@
-//
-//  MedicamentosView.swift
-//  perseus
-//
-//  Created by Luan Fazolin on 22/08/23.
-//
-
 import SwiftUI
+
+struct Medicamento: Identifiable {
+    var id: Int
+    let nome: String
+    let dosagem: String
+    let tempo: String
+}
+
+var medList = [
+    Medicamento(id: 0, nome: "Paracetamol", dosagem: "50mg", tempo: "2 comprimidos a cada 2 horas"),
+    Medicamento(id: 1, nome: "Ibuprofeno", dosagem: "100mg", tempo: "1 comprimidos a cada 8 horas")
+]
 
 struct MedicamentosView: View {
     var body: some View {
-        Text("Medicamentos View")
+        List(medList) { medicamento in
+            NavigationLink(destination: FichaView()) {
+                HStack {
+                    ZStack {
+                        Rectangle()
+                            .frame(width: 120, height: 120)
+                            .cornerRadius(10)
+                            .foregroundColor(Color(hex: 0xF2F2F7))
+                        
+                        Image("paracetamol")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                    }
+                    
+                    VStack(alignment: .leading) {
+                        Text(medicamento.nome)
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .multilineTextAlignment(.leading)
+                        
+                        Text(medicamento.dosagem)
+                            .font(.headline)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.gray)
+                            .multilineTextAlignment(.leading)
+                        
+                        Text(medicamento.tempo)
+                            .font(.subheadline)
+                            .multilineTextAlignment(.leading)
+                            .lineLimit(2)
+                    }
+                    .padding(.all)
+                }
+            }
+            .navigationBarItems(trailing:
+                    NavigationLink(destination: FichaView()) {
+                
+                    Image(systemName: "plus")
+                }
+            )
+        }
     }
 }
 
@@ -18,3 +64,4 @@ struct MedicamentosView_Previews: PreviewProvider {
         MedicamentosView()
     }
 }
+
