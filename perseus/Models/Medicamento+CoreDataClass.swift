99044e15b3d8159a8efdcff9ed4731f5183b50ca
Vitor Monzillo <66293065+vitormonzillo@users.noreply.github.com>
2023-08-30 09:26:13 -0300

    adiciona core data aos medicamentos

---
+++ perseus/Models/Medicamento+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Medicamento+CoreDataClass.swift
+//  perseus
+//
+//  Created by Vitor Monzillo on 30/08/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Medicamento)
+public class Medicamento: NSManagedObject {
+
+}
