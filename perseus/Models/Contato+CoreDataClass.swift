0becfdff849bfbfb0855101c1c5b15dd9dbaac6d
Vitor Monzillo <66293065+vitormonzillo@users.noreply.github.com>
2023-08-30 09:52:03 -0300

    adiciona core data e serviços aos contatos e o serviço do medicamento

---
+++ perseus/Models/Contato+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Contato+CoreDataClass.swift
+//  perseus
+//
+//  Created by Vitor Monzillo on 30/08/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Contato)
+public class Contato: NSManagedObject {
+
+}
