af9ffcc32cac7b361e70f46d782200e135d6cf5e
LPavarini <lucasfinzzeto@hotmail.com>
2023-08-31 15:35:22 -0300

    Save the view drug add 1

--- perseus/AdicaoMedicamentoView.swift
+++ perseus/AdicaoMedicamentoView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct AdicaoMedicamentoView: View {
     
+    @Environment(\.presentationMode) var presentationMode
+    
     @State var drugName: String = ""
     @State var dosagem: String = ""
     @State var quantidade: String = ""
@@ -16,6 +18,7 @@ struct AdicaoMedicamentoView: View {
     @State var observacoes: String = ""
     @State var imagem: String = ""
     
+    //@Environment(\.editMode) private var editMode
     
     var body: some View {
         VStack{
@@ -43,22 +46,36 @@ struct AdicaoMedicamentoView: View {
                         .autocorrectionDisabled()
                         .lineLimit(5, reservesSpace: true)
                 }
+
+                
                 Section{
-                    TextField("Imagem", text: $imagem, axis: .vertical)
-                        .autocorrectionDisabled()
-                        .lineLimit(7, reservesSpace: true)
-                    
-                Section{
-                    
+//                    if let data = elder.foto, let uiimage = UIImage(data: data) {
+//                        Image(uiImage: uiimage)
+//                            .resizable()
+//                            .scaledToFill()
+//                            .frame(width: 200, height: 180)
+//                            .clipShape(Circle())
+//                    } else {
+                        Image("paracetamol")
+                             .resizable()
+                             .scaledToFit()
+                             .frame(width: 200, height: 180)
+                    }
                 }
                 
-                }
-            }
-        }.navigationBarTitle("Medicamentos")
-            //.animation(nil, value: editMode?.wrappedValue)
-            .toolbar{
-                
-                EditButton()}
+        }
+        .navigationBarTitle("Medicamentos", displayMode: .inline)
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading:
+            Button("Cancelar", action: {
+            self.presentationMode.wrappedValue.dismiss()
+            
+            }))
+        .navigationBarItems(trailing:
+            Button("Salvar", action: {
+            
+            }))
+
     }
 }
 struct AdicaoMedicamentoView_Previews: PreviewProvider {
