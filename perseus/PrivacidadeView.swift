065e462cc596cf0c3e0b0d8e9a1c9137c865d7ec
Guilherme Lenzolari <103944988+guilenzolari@users.noreply.github.com>
2023-09-04 11:22:56 -0300

    atualizacoes nas politicas de privacidade

--- perseus/PrivacidadeView.swift
+++ perseus/PrivacidadeView.swift
@@ -11,11 +11,11 @@ struct PrivacidadeView: View {
     var body: some View {
         let privacyText = """
 Política de Privacidade
-Última Atualização: [Data da Última Atualização]
+Última Atualização: 04/09/2023
 
-    Na (nome empresarial simplificado), privacidade e segurança são prioridades e nos comprometemos com a transparência do tratamento de dados pessoais dos nossos usuários/clientes. Por isso, esta presente Política de Privacidade estabelece como é feita a coleta, uso e transferência de informações de clientes ou outras pessoas que acessam ou usam nosso site.
+    No aplicativo VERA CARE, privacidade e segurança são prioridades e nos comprometemos com a transparência do tratamento de dados pessoais dos nossos usuários/clientes. Por isso, esta presente Política de Privacidade estabelece como é feita a coleta, uso e transferência de informações de clientes ou outras pessoas que acessam ou usam nosso aplicativo.
     Ao utilizar nossos serviços, você entende que coletaremos e usaremos suas informações pessoais nas formas descritas nesta Política, sob as normas da Constituição Federal de 1988 (art. 5º, LXXIX; e o art. 22º, XXX – incluídos pela EC 115/2022), das normas de Proteção de Dados (LGPD, Lei Federal 13.709/2018), das disposições consumeristas da Lei Federal 8078/1990 e as demais normas do ordenamento jurídico brasileiro aplicáveis.
-    Dessa forma, a (nome empresarial), inscrita no CNPJ/MF sob o nº (nº do CNPJ), no papel de Controladora de Dados, obriga-se ao disposto na presente Política de Privacidade.
+    Dessa forma, o aplicativo VERA CARE, obriga-se ao disposto na presente Política de Privacidade.
 
     1. Quais dados coletamos sobre você e para qual finalidade?
     Nosso aplicativo coleta e utiliza alguns dados pessoais seus, de forma a viabilizar a prestação de serviços e aprimorar a experiência de uso.
@@ -27,14 +27,14 @@ Política de Privacidade
             Opção de foto de perfil, para identificação visual dentro do aplicativo.
 
         1.1.2 Informações de Saúde:
-            Essas informações são utilizadas apenas para melhorar e trazer serviços e             funcionalidades relacionadas à saúde:
+            Essas informações são utilizadas apenas para melhorar e trazer serviços e funcionalidades relacionadas à saúde:
             Tipo sanguíneo,
             Histórico de cirurgias,
             Doenças
             Alergias.
 
         1.1.3 Dados do boletim:
-            Essas informações são utilizadas apenas para gerar os gráficos e histórico             de anotações do parâmetro fornecido:
+            Essas informações são utilizadas apenas para gerar os gráficos e histórico de anotações do parâmetro fornecido:
             Pressão
             Temperatura
             Glicemia
@@ -43,10 +43,10 @@ Política de Privacidade
             Lucidez
             
         1.1.4 Medicamentos:
-            Mantemos uma lista dos medicamentos que você informa ser utilizado.                 Esses dados nos permitem fornecer alertas e lembretes relacionados aos             medicamentos, conforme configurado por você.
+            Mantemos uma lista dos medicamentos que você informa ser utilizado. Esses dados nos permitem fornecer alertas e lembretes relacionados aos medicamentos, conforme configurado por você.
 
         1.1.5 Contatos de Emergência:
-            Você tem a opção de fornecer nomes e números de telefone de contatos de         emergência. Essas informações são usadas exclusivamente para ligações e             contatos emergenciais
+            Você tem a opção de fornecer nomes e números de telefone de contatos de emergência. Essas informações são usadas exclusivamente para ligações e contatos emergenciais
 
     1.2. Dados pessoais coletados automaticamente
         Toda vez que o usuário cadastra algum dado, os únicos registros automáticos são de data e hora, para que cada dado posteriormente seja passível de filtros e ordenação.
@@ -59,41 +59,40 @@ Política de Privacidade
     Ao cadastrar um contato, os dados fornecidos são coletados
 
    2.1. Consentimento
-       É a partir do seu consentimento que tratamos os seus dados pessoais. O consentimento é a manifestação livre, informada e inequívoca pela qual você autoriza a (nome empresarial simplificado) a tratar seus dados.
+       É a partir do seu consentimento que tratamos os seus dados pessoais. O consentimento é a manifestação livre, informada e inequívoca pela qual você autoriza o app VERA CARE a tratar seus dados.
        Assim, em consonância com a Lei Geral de Proteção de Dados, seus dados só serão coletados, tratados e armazenados mediante prévio e expresso consentimento.
-       O seu consentimento será obtido de forma específica para cada finalidade acima descrita, evidenciando o compromisso de transparência e boa-fé da (nome empresarial simplificado) para com seus usuários, seguindo as regulações legislativas pertinentes.
-       Ao utilizar os serviços da (nome empresarial simplificado) e fornecer seus dados pessoais, você está ciente e consentindo com as disposições desta Política de Privacidade, além de conhecer seus direitos e como exercê-los.
+       O seu consentimento será obtido de forma específica para cada finalidade acima descrita, evidenciando o compromisso de transparência e boa-fé do app VERA CARE para com seus usuários, seguindo as regulações legislativas pertinentes.
+       Ao utilizar os serviços do app VERA CARE e fornecer seus dados pessoais, você está ciente e consentindo com as disposições desta Política de Privacidade, além de conhecer seus direitos e como exercê-los.
        A qualquer tempo e sem nenhum custo, você poderá revogar seu consentimento.
    É importante destacar que a revogação do consentimento para o tratamento dos dados pode implicar a impossibilidade da performance adequada de alguma funcionalidade do aplicativo que dependa da operação. Tais consequências serão informadas previamente.
 
    3. Quais são os seus direitos?
-       A (nome empresarial simplificado) assegura a seus usuários/clientes seus direitos de titular previstos no artigo 18 da Lei Geral de Proteção de Dados. Dessa forma, você pode, de maneira gratuita e a qualquer tempo:
+       O app VERA CARE assegura a seus usuários/clientes seus direitos de titular previstos no artigo 18 da Lei Geral de Proteção de Dados. Dessa forma, você pode, de maneira gratuita e a qualquer tempo:
    Confirmar a existência de tratamento de dados, de maneira simplificada ou em formato claro e completo.
    Acessar seus dados, podendo solicitá-los em uma cópia legível sob forma impressa ou por meio eletrônico, seguro e idôneo.
    Corrigir seus dados, ao solicitar a edição, correção ou atualização destes.
    Limitar seus dados quando desnecessários, excessivos ou tratados em desconformidade com a legislação através da anonimização, bloqueio ou eliminação.
-   Solicitar a portabilidade de seus dados, através de um relatório de dados cadastrais que a (nome empresarial simplificado) trata a seu respeito.
+   Solicitar a portabilidade de seus dados, através de um relatório de dados cadastrais que o app VERA CARE trata a seu respeito.
    Eliminar seus dados tratados a partir de seu consentimento, exceto nos casos previstos em lei.
    Revogar seu consentimento, desautorizando o tratamento de seus dados.
    Informar-se sobre a possibilidade de não fornecer seu consentimento e sobre as consequências da negativa.
 
    4. Como você pode exercer seus direitos de titular?
-       Para exercer seus direitos de titular, você deve entrar em contato com a (nome empresarial simplificado) através dos seguintes meios disponíveis:
-    Meio de contato
-    Meio de contato
+       Para exercer seus direitos de titular, você deve entrar em contato com a equipe de desenvolvimento do app VERA CARE através dos seguintes meios disponíveis:
+    glenzolari23@ecps.idserve.net
 
        De forma a garantir a sua correta identificação como titular dos dados pessoais objeto da solicitação, é possível que solicitemos documentos ou demais comprovações que possam comprovar sua identidade. Nessa hipótese, você será informado previamente.
 
    5. Como e por quanto tempo seus dados serão armazenados?
-       Seus dados pessoais coletados pela (nome empresarial simplificado) serão utilizados e armazenados durante o tempo necessário para a prestação do serviço ou para que as finalidades elencadas na presente Política de Privacidade sejam atingidas, considerando os direitos dos titulares dos dados e dos controladores.
-       De modo geral, seus dados serão mantidos enquanto a relação contratual entre você e a (nome empresarial simplificado) perdurar. Findado o período de armazenamento dos dados pessoais, estes serão excluídos de nossas bases de dados ou anonimizados, ressalvadas as hipóteses legalmente previstas no artigo 16 lei geral de proteção de dados, a saber:
+       Seus dados pessoais coletados pelo app VERA CASE serão utilizados e armazenados durante o tempo necessário para a prestação do serviço ou para que as finalidades elencadas na presente Política de Privacidade sejam atingidas, considerando os direitos dos titulares dos dados e dos controladores.
+       De modo geral, seus dados serão mantidos enquanto a relação contratual entre você e o app VERA CARE perdurar. Findado o período de armazenamento dos dados pessoais, estes serão excluídos de nossas bases de dados ou anonimizados, ressalvadas as hipóteses legalmente previstas no artigo 16 lei geral de proteção de dados, a saber:
        I – cumprimento de obrigação legal ou regulatória pelo controlador;
-       II – estudo por órgão de pesquisa, garantida, sempre que possível, a anonimização     dos dados pessoais;
-       III – transferência a terceiro, desde que respeitados os requisitos de tratamento de         dados dispostos nesta Lei; ou
-       IV – uso exclusivo do controlador, vedado seu acesso por terceiro, e desde que             anonimizados os dados.
+       II – estudo por órgão de pesquisa, garantida, sempre que possível, a anonimização dos dados pessoais;
+       III – transferência a terceiro, desde que respeitados os requisitos de tratamento de dados dispostos nesta Lei; ou
+       IV – uso exclusivo do controlador, vedado seu acesso por terceiro, e desde que anonimizados os dados.
 
        Isto é, informações pessoais sobre você que sejam imprescindíveis para o cumprimento de determinações legais, judiciais e administrativas e/ou para o exercício do direito de defesa em processos judiciais e administrativos serão mantidas, a despeito da exclusão dos demais dados.
-       O armazenamento de dados coletados pela (nome empresarial simplificado) reflete o nosso compromisso com a segurança e privacidade dos seus dados. Empregamos medidas e soluções técnicas de proteção aptas a garantir a confidencialidade, integridade e inviolabilidade dos seus dados. Além disso, também contamos com medidas de segurança apropriadas aos riscos e com controle de acesso às informações armazenadas.
+       O armazenamento de dados coletados pelo app VERA CARE reflete o nosso compromisso com a segurança e privacidade dos seus dados. Empregamos medidas e soluções técnicas de proteção aptas a garantir a confidencialidade, integridade e inviolabilidade dos seus dados. Além disso, também contamos com medidas de segurança apropriadas aos riscos e com controle de acesso às informações armazenadas.
 
    6. O que fazemos para manter seus dados seguros?
        Para mantermos suas informações pessoais seguras, usamos ferramentas físicas, eletrônicas e gerenciais orientadas para a proteção da sua privacidade.
@@ -102,44 +101,45 @@ Política de Privacidade
    Apenas pessoas autorizadas têm acesso a seus dados pessoais
    O acesso a seus dados pessoais é feito somente após o compromisso de confidencialidade
    Seus dados pessoais são armazenados em ambiente seguro e idôneo.
-       A (nome empresarial simplificado) se compromete a adotar as melhores posturas para evitar incidentes de segurança. Contudo, é necessário destacar que nenhuma página virtual é inteiramente segura e livre de riscos. É possível que, apesar de todos os nossos protocolos de segurança, problemas de culpa exclusivamente de terceiros ocorram, como ataques cibernéticos de hackers, ou também em decorrência da negligência ou imprudência do próprio usuário/cliente.
+       o aplicativo VERA CARE se compromete a adotar as melhores posturas para evitar incidentes de segurança. Contudo, é necessário destacar que nenhuma página virtual é inteiramente segura e livre de riscos. É possível que, apesar de todos os nossos protocolos de segurança, problemas de culpa exclusivamente de terceiros ocorram, como ataques cibernéticos de hackers, ou também em decorrência da negligência ou imprudência do próprio usuário/cliente.
        Em caso de incidentes de segurança que possa gerar risco ou dano relevante para você ou qualquer um de nossos usuários/clientes, comunicaremos aos afetados e a Autoridade Nacional de Proteção de Dados sobre o ocorrido, em consonância com as disposições da Lei Geral de Proteção de Dados.
 
    7. Com quem seus dados podem ser compartilhados?
-       Tendo em vista a preservação de sua privacidade, a (nome empresarial simplificado) não compartilhará seus dados pessoais com nenhum terceiro não autorizado.
+       Tendo em vista a preservação de sua privacidade, o app VERA CARE não compartilhará seus dados pessoais com nenhum terceiro não autorizado.
        Existem algumas hipóteses em que seus dados poderão ser compartilhados:
    I – Determinação legal, requerimento, requisição ou ordem judicial, com autoridades judiciais, administrativas ou governamentais competentes.
    II – Caso de movimentações societárias, como fusão, aquisição e incorporação, de forma automática
-   III – Proteção dos direitos da (nome empresarial simplificado) em qualquer tipo de conflito, inclusive os de teor judicial.
+   III – Proteção dos direitos do app VERA CARE em qualquer tipo de conflito, inclusive os de teor judicial.
 
    7.1. Transferência internacional de dados
        Alguns dos terceiros com quem compartilhamos seus dados podem ser localizados ou ou possuir instalações localizadas em países estrangeiros. Nessas condições, de toda forma, seus dados pessoais estarão sujeitos à Lei Geral de Proteção de Dados e às demais legislações brasileiras de proteção de dados
-       Nesse sentido, a (nome empresarial simplificado) se compromete a sempre adotar eficientes padrões de segurança cibernética e de proteção de dados, nos melhores esforços de garantir e cumprir as exigências legislativas.
+       Nesse sentido, o app VERA CARE se compromete a sempre adotar eficientes padrões de segurança cibernética e de proteção de dados, nos melhores esforços de garantir e cumprir as exigências legislativas.
        Ao concordar com essa Política de Privacidade, você concorda com esse compartilhamento, que se dará conforme as finalidades descritas no presente instrumento.
 
    8. Alteração desta Política de Privacidade
-       A atual versão da Política de Privacidade foi formulada e atualizada pela última vez em: (data da última atualização da Política de Privacidade).
+       A atual versão da Política de Privacidade foi formulada e atualizada pela última vez em: 04/09/2023.
        Reservamos o direito de modificar essa Política de Privacidade a qualquer tempo, principalmente em função da adequação a eventuais alterações feitas em nosso site ou em âmbito legislativo. Recomendamos que você a revise com frequência.
        Eventuais alterações entrarão em vigor a partir de sua publicação em nosso site e sempre lhe notificaremos acerca das mudanças ocorridas.
        Ao utilizar nossos serviços e fornecer seus dados pessoais após tais modificações, você as consente.
    9. Responsabilidade
-       A (nome empresarial simplificado) prevê a responsabilidade dos agentes que atuam nos processos de tratamento de dados, em conformidade com os artigos 42 ao 45 da Lei Geral de Proteção de Dados.
+       O app VERA CARE prevê a responsabilidade dos agentes que atuam nos processos de tratamento de dados, em conformidade com os artigos 42 ao 45 da Lei Geral de Proteção de Dados.
        Nos comprometemos em manter esta Política de Privacidade atualizada, observando suas disposições e zelando por seu cumprimento.
        Além disso, também assumimos o compromisso de buscar condições técnicas e organizativas seguramente aptas a proteger todo o processo de tratamento de dados.
-       Caso a Autoridade Nacional de Proteção de Dados exija a adoção de providências em relação ao tratamento de dados realizado pela (nome empresarial simplificado), comprometemo-nos a segui-las.
+       Caso a Autoridade Nacional de Proteção de Dados exija a adoção de providências em relação ao tratamento de dados realizado pelo app VERA CARE, comprometemo-nos a segui-las.
 
    9.1 Isenção de responsabilidade
-       Conforme mencionado no Tópico 6, embora adotemos elevados padrões de segurança a fim de evitar incidentes, não há nenhuma página virtual inteiramente livre de riscos. Nesse sentido, a (nome empresarial simplificado) não se responsabiliza por:
+       Conforme mencionado no Tópico 6, embora adotemos elevados padrões de segurança a fim de evitar incidentes, não há nenhum aplivativo inteiramente livre de riscos. Nesse sentido, o app VERA CARE não se responsabiliza por:
    I – Quaisquer consequências decorrentes da negligência, imprudência ou imperícia dos usuários em relação a seus dados individuais. Garantimos e nos responsabilizamos apenas pela segurança dos processos de tratamento de dados e do cumprimento das finalidades descritas no presente instrumento.
    Destacamos que a responsabilidade em relação à confidencialidade dos dados de acesso é do usuário.
-   II – Ações maliciosas de terceiros, como ataques de hackers, exceto se comprovada conduta culposa ou deliberada da (nome empresarial simplificado).
+   II – Ações maliciosas de terceiros, como ataques de hackers, exceto se comprovada conduta culposa ou deliberada do app VERA CARE.
    Destacamos que em caso de incidentes de segurança que possam gerar risco ou dano relevante para você ou qualquer um de nossos usuários/clientes, comunicaremos aos afetados e a Autoridade Nacional de Proteção de Dados sobre o ocorrido e cumpriremos as providências necessárias.
-   III – Inveracidade das informações inseridas pelo usuário/cliente nos registros necessários para a utilização dos serviços da (nome empresarial simplificado); quaisquer consequências decorrentes de informações falsas ou inseridas de má-fé são de inteiramente responsabilidade do usuário/cliente.
+   III – Inveracidade das informações inseridas pelo usuário/cliente nos registros necessários para a utilização dos serviços do app VERA CARE; quaisquer consequências decorrentes de informações falsas ou inseridas de má-fé são de inteiramente responsabilidade do usuário/cliente.
 
    11. Encarregado de Proteção de Dados
-       A (nome empresarial simplificado) disponibiliza os seguintes meios para que você possa entrar em contato conosco para exercer seus direitos de titular: (meios de contato).
+       O aplicativo VERA CARE disponibiliza os seguintes meios para que você possa entrar em contato conosco para exercer seus direitos de titular: glenzolari23@ecps.idserve.net.
    Caso tenha dúvidas sobre esta Política de Privacidade ou sobre os dados pessoais que tratamos, você pode entrar em contato com o nosso Encarregado de Proteção de Dados Pessoais, através dos seguintes canais:
-   [Email do Encarregado]
+      glenzolari23@ecps.idserve.net
+
 
 """
         VStack{
